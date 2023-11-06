bb151fed988b55ac893052c25fc742e286c6f36c
0x7a726f <69540160+0x7a726f@users.noreply.github.com>
2023-11-06 23:01:26 +0200

    Add files via upload

---
+++ prog2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int a1,a2,a3,a4,a5,a6,a7,a8;
+int i,j,n;
+int powermatrix[10] = {0,1,2,3,4,5,6,7,8,9};
+int main(){
+    cin >> n;
+    for(i=0;i<=9;i++){
+        powermatrix[i] = pow(powermatrix[i],n);
+    }
+    for(a1=0;a1<=9;a1++){
+        for(a2=0;a2<=9;a2++){
+            for(a3=0;a3<=9;a3++){
+                for(a4=0;a4<=9;a4++){
+                    for(a5=0;a5<=9;a5++){
+                        for(a6=0;a6<=9;a6++){
+                            for(a7=0;a7<=9;a7++){
+                                for(a8=0;a8<=9;a8++){
+                                    int number = (a8+10*a7+100*a6+1000*a5+10000*a4+100000*a3+1000000*a2+10000000*a1);
+                                    int sum = (powermatrix[a1]+powermatrix[a2]+powermatrix[a3]+powermatrix[a4]+powermatrix[a5]+powermatrix[a6]+powermatrix[a7]+powermatrix[a8]);
+                                    if( number == sum){
+                                        cout << number << endl;
+                                    }  
+                                }
+                            }
+                        }
+                    }
+                }
+            } 
+        } 
+    }
+}

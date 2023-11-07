170b6de4b91fefeb8e80889bb3b5d337d509dbf4
0x7a726f <69540160+0x7a726f@users.noreply.github.com>
2023-11-07 23:53:16 +0200

    Add files via upload

---
+++ prog3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <algorithm> 
+using namespace std;
+int n,i;
+float result = 0;
+string palindrome = "";
+int PalindromeLength;
+int main(){
+    int n;
+    cin >> n;
+    cin.sync();
+    for(i=1;i<=n;i++){
+        string palindrome = "";
+        getline(cin,palindrome);
+        if(palindrome.empty()){cout << "empty";continue;}
+        PalindromeLength = palindrome.length();
+        if(PalindromeLength > 20){
+            cout << "error";
+            continue;
+        }
+        string temp = palindrome;
+        reverse(palindrome.begin(), palindrome.end()); 
+        if(temp == palindrome){
+            cout << "yes";
+            result = result + 1.0;
+            continue;
+        }
+        else{
+            cout << "no";
+            continue;
+        }
+    }
+    float a = result/n;
+    cout << a*100.000;
+
+}

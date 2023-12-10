0a9b6e5e5c5b805e82b51c781139bd3c4ca32f5d
0x7a726f <69540160+0x7a726f@users.noreply.github.com>
2023-12-11 01:31:18 +0200

    Delete seira7 directory

--- seira7/prog2.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int num;
-    cin >> num;
-    if(((num % 6) != 0 ) && ((num % 7 ) == 0)){cout << "yes" << endl;}
-    else{cout << "no" << endl;}
-}
--- seira7/prog3.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int num;
-    cin >> num;
-    if(num>99 && num <1000){cout << "yes" << endl;}
-    else{cout << "no" << endl;}
-}

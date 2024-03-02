9b823b6737854999532dcdc5758e706079f2905b
0x7a726f <69540160+0x7a726f@users.noreply.github.com>
2024-03-02 17:16:51 +0200

    Create babyratio.hpp

---
+++ tech/seira1/babyratio.hpp
@@ -0,0 +1,12 @@
+class rational {
+    public:
+        rational(int n, int d);
+        rational add(rational r);
+        rational sub(rational r);
+        rational mul(rational r);
+        rational div(rational r);
+        void print();
+    private:
+        int nom, den; // nominator, denominator
+        static int gcd(int a, int b);
+};

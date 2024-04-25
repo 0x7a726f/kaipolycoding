4e11e4c80964f84b318db45662cfba74431beca4
0x7a726f <69540160+0x7a726f@users.noreply.github.com>
2024-04-26 01:40:09 +0300

    Add files via upload

---
+++ tech/euler.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int vertex;
+    int edge;
+    cin >> vertex >> edge;
+    vector<int> matrix(vertex,0);
+    for(int i = 0;i<edge;i++){
+        int temp;
+        cin >> temp;
+        matrix[temp]++;
+        cin >> temp;
+        matrix[temp]++;
+    }
+    int res1 = -1;
+    int res2 = -1;
+    int counter = 0;
+    for(int i = 0;i<vertex;i++){
+        if(matrix[i]%2 == 1){
+            if(counter < 2){res1 == -1 ? res1 = i : res2 = i;counter++;}
+            else{counter = 10;break;}
+        }
+    }
+    if(counter>2 || counter == 1){cout << "IMPOSSIBLE\n";}
+    else if(counter == 0){cout << "CYCLE\n";}
+    else{cout << "PATH ";res1 < res2 ? cout << res1 << " " << res2 : cout << res2 << " " << res1;cout << endl;}
+}

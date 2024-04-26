6e13f7236f439a78c67e42fc6e0ea564172879bb
0x7a726f <69540160+0x7a726f@users.noreply.github.com>
2024-04-26 22:47:47 +0300

    Update cycle.cpp

--- tech/seira5/cycle.cpp
+++ tech/seira5/cycle.cpp
@@ -3,8 +3,30 @@
 using namespace std;
 class Graph {
     public:
-        Graph(int V);
-        ~Graph();
-        void addEdge(int u, int v);
-        bool cycle(vector<int> &path) const;
+        Graph(int V){
+            size = V;
+            VertexList = new node*[V];
+            for(int i = 0;i < V;i++){
+                VertexList[i] = new node;
+            }
+        }
+
+        ~Graph(){
+            for(int i = 0;i < size;i++){
+                delete VertexList[i];
+            }
+            delete [] VertexList;
+        }
+
+        void addEdge(int u, int v){
+            VertexList[u]->AdjList.push_back(v);
+        }
+
+        //bool cycle(vector<int> &path) const;
+    private:
+        int size;
+        struct node{
+            vector<int> AdjList;
+        };
+        node** VertexList;   
 };
